66bfd56ed47d79f63831e42891c30e98dd670016
SmitWarangZeus <smit.warang@zeuslearning.com>
2026-06-03 16:29:50 +0530

    fixed btn issues and removed innerhtml

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// localStorage.removeItem('tasks');
 let tasks=JSON.parse(localStorage.getItem('tasks'))||[];
 let newId=0;
 
@@ -29,23 +30,60 @@ function addTask(){
 
 function displayTasks(){
     let tbody=document.getElementById('table-body');
-    tbody.innerHTML=''
+    tbody.innerHTML='';
     tasks.sort((a,b)=>new Date(a.dueDate)-new Date(b.dueDate));
     tasks.forEach((task,index)=>{
+        let row=document.createElement('tr');
         let btnclass=task.status==='Complete'?'btn-com':'btn-incom'
-        tbody.innerHTML+=`
-        <tr>
-            <td>${task.name}</td>
-            <td>${task.desc}</td>
-            <td>${task.dueDate}</td>
-            <td><button class=${btnclass} onClick='toggle(${task.taskId})'>${task.status}</button></td>
-            <td><button class='del-btn' onClick='deleteTask(${task.taskId})'>Delete</button></td>
-        </tr>`
-    });
+
+        let nameCell=document.createElement('td');
+        nameCell.textContent=task.name;
+        row.appendChild(nameCell);
+
+        let descCell=document.createElement('td');
+        descCell.textContent=task.desc;
+        row.appendChild(descCell);
+
+        let dateCell=document.createElement('td');
+        dateCell.textContent=task.dueDate;
+        row.appendChild(dateCell);
+
+        let toggleCell=document.createElement('td');
+        let toggleBtn=document.createElement('button');
+        toggleBtn.textContent=task.status;
+        toggleBtn.onclick=()=>toggle(task.taskId);
+        toggleBtn.classList.add(btnclass);
+        toggleCell.appendChild(toggleBtn);
+        row.appendChild(toggleCell);
+
+        let deleteCell=document.createElement('td');
+        let deleteBtn=document.createElement('button');
+        deleteBtn.textContent='Delete';
+        deleteBtn.onclick=()=>deleteTask(task.taskId);
+        deleteBtn.classList.add('del-btn');
+        deleteCell.appendChild(deleteBtn);
+        row.appendChild(deleteCell);
+
+        tbody.appendChild(row);
+    })
+
+
+    // tbody.innerHTML=''
+    // tasks.sort((a,b)=>new Date(a.dueDate)-new Date(b.dueDate));
+    // tasks.forEach((task,index)=>{
+    //     let btnclass=task.status==='Complete'?'btn-com':'btn-incom'
+    //     tbody.innerHTML+=`
+    //     <tr>
+    //         <td>${task.name}</td>
+    //         <td>${task.desc}</td>
+    //         <td>${task.dueDate}</td>
+    //         <td><button class=${btnclass} onClick='toggle(${task.taskId})'>${task.status}</button></td>
+    //         <td><button class='del-btn' onClick='deleteTask(${task.taskId})'>Delete</button></td>
+    //     </tr>`
+    // });
 }
 
 function deleteTask(id){
-    console.log(tasks,id);
     tasks.forEach((task,index)=>{
         if (task.taskId===id){
             tasks.splice(index,1);
